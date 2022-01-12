dbd4c9dc70cf896075bcb8ea6b699ef8c9e82a3c
omarturmun <93262369+omarturmun@users.noreply.github.com>
2022-01-12 14:23:44 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("pozik")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("biotza")
+})
+radio.onReceivedString(function (receivedString) {
+    if ("pozik" == receivedString) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if ("triste" == receivedString) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if ("biotza" == receivedString) {
+        basic.showIcon(IconNames.Heart)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("triste")
+})
+radio.setGroup(6)
+basic.forever(function () {
+	
+})
